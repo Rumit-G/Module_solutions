49a1417e32e4aec3ed8b8edbcf4be228712b4278
Rumit-G <95767338+Rumit-G@users.noreply.github.com>
2022-01-08 18:48:02 +0530

    Update script.js
    minor change

--- module_4_solution/js/script.js
+++ module_4_solution/js/script.js
@@ -12,10 +12,6 @@
     "Jim",
   ];
 
-  // STEP 10:
-  // Loop over the names array and say either 'Hello' or "Good Bye"
-  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
-  // See Lecture 50, part 1
   for (var i = 0; i < names.length; i++) {
     var firstletter = names[i].charAt(0).toLowerCase();
     if (firstletter === "j") {
